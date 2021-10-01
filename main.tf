e4d16a692a17f5da4ceda49877a3bf6c3ee9a3f4
ion <ion@hashicorp.com>
2021-10-01 11:09:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ terraform {
 provider "null" {
   # Configuration options
 }
+
+resource "null_resource" "cluster" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.my-pet.id}"
+    
+  }
+}
+resource "random_pet" "my-pet" {
+  prefix = "Mr"
+  length = 1
+}
